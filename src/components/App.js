22af972cc68e5f5fc3a2d8917d7c766cb6fa7c35
girishyadav <girishayadav.com>
2024-07-12 13:57:13 +0530

    first chnage

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,33 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useEffect, useState } from 'react'
+import Markdown from "markdown-to-jsx";
+import "../styles/App.css";
 
+
+const App = () => {
+    const [value, setValue] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const MarkdownPreview = () => <Markdown>{value}</Markdown>
+    const handleChange= (e) => {
+        setValue(e.target.value);
+    }
+
+    const Loading = () => <h1 className='loading'>Loading...</h1>
+
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 1000)
+    }, [value]);
+  return (
+    <div className='app'>
+        <textarea className='textarea' value={value} onChange={handleChange}></textarea>
+        <div className='preview'>
+            {loading ? <Loading/> : <MarkdownPreview/>}
+        </div>
+    </div>
+  )
+}
+
+export default App
